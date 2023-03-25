ef6cec34b78e07f0bab489d2a9c0e684a0a367ba
Christopher Mena <mena.christopher.student@gmail.com>
2023-03-25 11:00:17 -0400

    Started React Testing App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <ImageButtonContainer />
+      <Results />
+    </>
+  );
+}
+
+const ImageSelect = () => {
+  return (
+    <div className="file-input form__input">
+      <input type="file" id="image-selector" accept="image/*"></input>
     </div>
+  );
+}
+
+const InputButton = () => {
+  return (
+    <div className="button-input form__input">
+      <button id="predictBtn">Predict</button>
+    </div>
+  );
+}
+
+const ImageButtonContainer = () => {
+  return <div className="input-container form">
+  <ImageSelect />
+  <InputButton />
+  </div>
+}
+
+const Results = () => {
+  return (
+    <div className="results">
+    <div className="result-in">
+      <div className="result-image">
+        <h2>Image</h2>
+        <img id="selected-image" className="ml-3" width="250" alt="" />
+      </div>
+      <div className="result-list">
+        <ol id="list"></ol>
+      </div>
+    </div>
+  </div>
   );
 }
 
